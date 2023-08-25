de673fdcafe2c990e8f5e656b1d9afaf490f0ee5
Rajaonarison <rajaonarison.moreno@gmail.com>
2023-08-25 11:36:25 +0200

    feat: create auth middleware

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,12 +1,21 @@
 import express from "express";
-import { login, register, logout, refreshtoken } from "../controllers/auth.controller.js";
+import {
+  login,
+  register,
+  logout,
+  refreshtoken,
+} from "../controllers/auth.controller.js";
 import { all } from "trim-request";
+import authMiddleware from "../middlewares/auth.middleware.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/login", all, login);
 authRouter.post("/register", all, register);
-authRouter.post("/logout", all, logout)
-authRouter.post("/refreshtoken", all, refreshtoken)
+authRouter.post("/logout", all, logout);
+authRouter.post("/refreshtoken", all, refreshtoken);
+authRouter.get("/testmiddleware", authMiddleware, (req, res) => {
+  res.send(req.user);
+});
 
 export default authRouter;
