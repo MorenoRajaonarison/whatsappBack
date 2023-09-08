5da8dbdcbbaf5b65106d9e4bec0616d020cc80a8
Rajaonarison <rajaonarison.moreno@gmail.com>
2023-09-08 08:33:34 +0200

    feat: find or create conversation

--- src/controllers/conversation.controller.js
+++ src/controllers/conversation.controller.js
@@ -1,21 +1,48 @@
-import createHttpError from "http-errors"
-import logger from "../configs/logger.config.js"
-import { doesConversationExist } from "../services/conversation.service.js"
+import createHttpError from "http-errors";
+import logger from "../configs/logger.config.js";
+import {
+  createConversation,
+  doesConversationExist,
+} from "../services/conversation.service.js";
+import { findUser } from "../services/user.service.js";
 
 export const createOrOpenConversation = async (req, res, next) => {
   try {
-    const senderId = req.user.userId
-    const {receiverId} = req.body
+    const senderId = req.user.userId;
+    const { receiverId } = req.body;
 
     // check if receiverId is provide
-    if(!receiverId) {
-      logger.error('Please provide the user id you wanna start a conversation with !')
-      throw createHttpError.BadGateway('Something went wrong !')
+    if (!receiverId) {
+      logger.error(
+        "Please provide the user id you wanna start a conversation with !"
+      );
+      throw createHttpError.BadGateway("Oops... Something went wrong !");
     }
 
     // check if chat exist
-    const existedConversation = await doesConversationExist(senderId, receiverId)
+    const existedConversation = await doesConversationExist(
+      senderId,
+      receiverId
+    );
+
+    if (existedConversation) {
+      res.json(existedConversation);
+    } else {
+      let receiverUser = await findUser(receiverId);
+      let convoData = {
+        name: receiverUser.name,
+        isGroup: false,
+        users: [senderId, receiverId],
+      };
+      const newConvo = await createConversation(convoData);
+      const populatedConvo = await populateConvo(
+        newConvo._id,
+        "users",
+        "-password"
+      );
+      res.status(200).json(populatedConvo);
+    }
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
--- src/services/conversation.service.js
+++ src/services/conversation.service.js
@@ -1,1 +1,41 @@
-export const doesConversationExist = async (senderId, receiverId) => {};
+import { ConversationModel, UserModel } from "../models/index.js";
+
+export const doesConversationExist = async (senderId, receiverId) => {
+  let convos = await ConversationModel.find({
+    isGroup: false,
+    $and: [
+      { users: { $elemMatch: { $eq: senderId } } },
+      { users: { $elemMatch: { $eq: receiverId } } },
+    ],
+  })
+    .populate("users", "-password")
+    .populate("latestMessage");
+
+  if (!convos)
+    throw createHttpError.BadRequest("Oops... Something went wrong !");
+
+  // populate message model
+
+  convos = await UserModel.populate(convos, {
+    path: "latestMessage.sender",
+    select: "name email picture status",
+  });
+  return convos[0];
+};
+
+export const createConversation = async (data) => {
+  const newConvo = await ConversationModel.create(data);
+  if (!newConvo)
+    throw createHttpError.BadRequest("Oops... Something went wrong !");
+  return newConvo;
+};
+
+export const populateConvo = async (id, fieldToPopulate, fieldToRemove) => {
+  const populatedConvo = await ConversationModel.findOne({ _id: id }).populate(
+    fieldToPopulate,
+    fieldToRemove
+  );
+  if (!populatedConvo)
+    throw createHttpError.BadRequest("Oops... Something went wrong !");
+  return populatedConvo;
+};
