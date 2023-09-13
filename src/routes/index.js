87314e969639e9b0c58823386da05b65bd8782fb
Rajaonarison <rajaonarison.moreno@gmail.com>
2023-09-13 15:41:56 +0200

    feat: search user for conversation

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import authRoutes from "./auth.router.js";
 import conversationRoutes from "./conversation.router.js";
 import messageRoute from "./message.route.js";
+import userRoute from "./user.router.js";
 
 const router = express.Router();
 router.use("/auth", authRoutes);
 router.use("/conversation", conversationRoutes);
+router.use("/user", userRoute);
 router.use("/message", messageRoute);
 
 export default router;
