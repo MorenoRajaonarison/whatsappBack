262579e5070156db8508c42696d73bd414ed5409
Rajaonarison <rajaonarison.moreno@gmail.com>
2023-09-21 16:51:08 +0200

    feat: send message and fix receiver name and image

--- src/SocketServer.js
+++ src/SocketServer.js
@@ -1,7 +1,7 @@
 export default (socket) => {
   // user joins or open the app
   socket.on("join", (user) => {
-    console.log('JOINED USER: ',user);
+    console.log("JOINED USER: ", user);
     socket.join(user);
   });
 
@@ -9,5 +9,15 @@ export default (socket) => {
   socket.on("joinConversation", (conversation) => {
     socket.join(conversation);
     console.log("JOINED CONVERSATION: ", conversation);
+  });
+
+  // send and receive message
+  socket.on("sendMessage", (message) => {
+    let conversation = message.conversation;
+    if (!conversation.users) return;
+    conversation.users.forEach((user) => {
+      if (user._id === message.sender._id) return;
+      socket.in(user._id).emit("messageReceived", message);
+    });
   });
 };
